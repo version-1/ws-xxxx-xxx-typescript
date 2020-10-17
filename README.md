b97d4f70f60dd267dc02079335eb1d1b447935a5
version-1 <j.onodera26@gmail.com>
2020-10-16 18:01:24 -0700

    some fix

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ws-xxxx-xxx-typescript
+
+1. コンパイル
+2. tsconfig
+3. TypeScript入門
+3. 型定義
+4. クラス定義
+5. インターフェース
+6. ジェネリクス
+7. 特殊な型宣言<null安全, undefined, null, Union>
--- index.ts
+++ index.ts
@@ -1,1 +1,51 @@
+// normal syntax
 console.log('hello world')
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+// string to number
+
+let hoge = 'string'
+
+hoge = 1
+hoge = true
+
+// array
+let numberSeq = [1, 2, 3, 4]
+
+numberSeq = [1, 2, 3]
+numberSeq = ['ichiro', 'jiro', 'sabro']
+
+// object
+let profile = { name: 'ichiro', age: 12 }
+
+profile = { name: 'fuga', age: 13 }
+profile = { name: 'fuga', age: 13, gender: 'male' }
+
+// Class
+class Human {
+  breeth() {
+    console.log('breeth')
+  }
+}
+
+class Robot {
+  run() {
+    console.log('run')
+  }
+}
+
+let something = new Human()
+
+something = new Robot()
+
+
+// Type Inference
+let greeding = "Good Morning"
+
+let greeding2: string = "Good Morning"
+
+// Null Safe
+
